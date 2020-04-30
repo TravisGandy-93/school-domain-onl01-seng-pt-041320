600f46d5e47cf490e0ccff3839723bc168d933c5
Travis Gandy <travis_gandy@yahoo.com>
2020-04-30 03:36:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,9 @@
 # code here!
 class School 
-  attr_reader :roster
   
-  def initialize(roster={})
-    @roster = roster
-  
+  def initialize(name)
+      @name = name
+      roster = {}
   end
   
 end 
